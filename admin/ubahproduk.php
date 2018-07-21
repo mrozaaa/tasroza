fea1e7941bd67293e50d19210988e62a8e9e6dce
mrozaaa <mrozaaa@gmail.com>
2018-07-21 21:26:54 +0700

    upload

---
+++ admin/ubahproduk.php
@@ -0,0 +1,57 @@
+<h2>Ubah Produk</h2>
+
+<?php  
+$ambil=$koneksi->query("SELECT * FROM produk WHERE id_produk='$_GET[id]'");
+$pecah= $ambil->fetch_assoc();
+
+echo "<pre>";
+print_r($pecah);
+echo "</pre>";
+?>
+
+<form method="post" enctype="multipart/form-data">
+	<div class="form-group">
+		<label>Nama Produk</label>
+		<input type="text" class="form-control" name="nama" value="<?php echo $pecah['nama_produk']; ?>">
+	</div>
+	<div class="form-group">
+		<label>Harga (Rp)</label>
+		<input type="number" class="form-control" name="harga" value="<?php echo $pecah['harga_produk']; ?>">
+	</div>
+	<div class="form-group">
+		<label>Berat</label>
+		<input type="number" class="form-control" name="berat" value="<?php echo $pecah['berat_produk']; ?>">
+	</div>
+	<div class="form-group">
+		<img src="../foto_produk/<?php echo $pecah['foto_produk'] ?>" width="200">
+	</div>
+	<div class="form-group">
+		<label>Ganti Foto</label>
+		<input type="file" class="form-control" name="foto">
+	</div>
+	<div class="form-group">
+		<label>Deskripsi</label>
+		<textarea class="form-control" name="deskripsi" rows="10"><?php echo $pecah['deskripsi_produk']; ?>
+		</textarea>	
+	</div>
+	<button class="btn btn-danger" name="ubah">Ubah</button>
+</form>
+<?php 
+if (isset($_POST['ubah'])) 
+{
+ 	$nama = $_FILES['foto']['name'];
+ 	$lokasi = $_FILES['foto']['tmp_name'];
+ 	if (!empty($lokasi)) 
+ 	{
+ 		move_uploaded_file($lokasi, "../foto_produk/".$nama);
+
+ 		$koneksi->query("UPDATE produk SET nama_produk='$_POST[nama]',harga_produk='$_POST[harga]',berat_produk='$_POST[berat]',foto_produk='$nama',deskripsi_produk='$_POST[deskripsi]' WHERE id_produk='$_GET[id]'");
+ 	}
+ 	else
+ 	{
+ 		$koneksi->query("UPDATE produk SET nama_produk='$_POST[nama]',harga_produk='$_POST[harga]',berat_produk='$_POST[berat]',deskripsi_produk='$_POST[deskripsi]' WHERE id_produk='$_GET[id]'");
+ 	}
+ 	echo "<script>alert('data produk telah diubah');</script>";
+ 	echo "<script>location='index.php?halaman=produk';</script>";
+}
+?>
